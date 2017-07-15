bfbf17c499fa9f0325e4a6b52f75a6d878d874fc
I Chan Kwon <ichankwon@Is-MacBook-Pro.local>
2017-07-15 15:02:58 -0700

    Fully Functioning Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,63 @@
 //Characters//
 $("#anakin-pic").data({
   "name": "Anakin Skywalker",
-  "hp": 120,
-  "ap": 8,
-  "cap": 15
+  "hp": 150,
+  "ap": 10,
+  "cap": 12,
+  "baseAp": 8
 });
 
 $("#obiwan-pic").data({
   "name": "Obi Wan Kenobi",
-  "hp": 150,
-  "ap": 6,
-  "cap": 20
+  "hp": 180,
+  "ap": 7,
+  "cap": 18
 });
 
 $("#griv-pic").data({
   "name": "General Grievious",
-  "hp": 180,
-  "ap": 5,
-  "cap": 18
+  "hp": 160,
+  "ap": 8,
+  "cap": 15
 });
 
 $("#palpatine-pic").data({
   "name": "Senator Palpatine",
-  "hp": 100,
-  "ap": 4,
+  "hp": 145,
+  "ap": 12,
   "cap": 12
 });
 
-//Game stage that prevents duplicate selections//
+//Game stage that prevents duplicate character selections//
 var gameStage = "choose-character";
+
+//Emtpy variable to store data//
+var currentAttackPower = 0;
+var currentCharacterHp = "";
+var currentEnemyHp = "";
+
+//Functions to reduce repetitive codes//
+function showContent() {
+  $("#yourEnemy").fadeIn();
+  $("#battle").fadeIn();
+  $("#attack-btn").fadeIn();
+}
+
+//Function that increments your character's attack power by the base power//
+function inBattle() {
+  currentAttackPower += charAttr.ap;
+  charAttr.hp -= enemyAttr.cap;
+  enemyAttr.hp -= currentAttackPower;
+  $("#characterAttr").html(charAttr.hp);
+  $("#enemyAttr").html(enemyAttr.hp);
+}
+
+//Upon defeating your enemy, hide the display boxes//
+function removeLoser() {
+  $("#enemyPic").empty();
+  $("#yourEnemy").fadeOut();
+  $("#battle").fadeOut();
+}
 
 //Game function//
 $(document).ready(function() {
@@ -41,58 +70,54 @@ $(document).ready(function() {
   $(".pause-button").on("click", function() {
     getAudio.pause();
   });
+  $("#logo").prop("onclick", false);
   //Display the character selection box upon load//
   $("#yourCharacter").hide();
   $("#yourEnemy").hide();
   $("#battle").hide();
   $("img").on("click", function() {
+
     //Player selects which character to play as//
     if (gameStage === "choose-character") {
       $("#characterPic").append(this);
       charAttr = $(this).data();
-      console.log(charAttr);
       $(this).css("width", "20%");
       $("#yourCharacter").fadeIn();
-      $("#characterAttr").text(charAttr.name + " / " + "HP= " + charAttr.hp);
       $("#choose").html("Choose Your Enemy For The First Battle");
+      $("#characterName").html(charAttr.name);
+      $("#characterAttr").html(charAttr.hp);
       gameStage = "choose-1st-enemy";
 
       //Player selects the 1st enemy to fight//
     } else if (gameStage === "choose-1st-enemy") {
       $("#enemyPic").append(this);
       enemyAttr = $(this).data();
-      console.log(enemyAttr);
+      showContent();
       $(this).css("width", "20%");
-      $("#yourEnemy").fadeIn();
-      $("#battle").fadeIn();
-      $("#attack-btn").fadeIn();
-      $("#enemyAttr").text(enemyAttr.name + " / " + "HP= " + enemyAttr.hp);
-      $("#choose").html("Your Enemies Awating For The Next Battle");
+      $("#choose").html("Fight Your Enemy By Clicking The Attack Button!");
+      $("#enemyName").html(enemyAttr.name);
+      $("#enemyAttr").html(enemyAttr.hp);
       gameStage = "battle-1";
 
       //Player selects the 2nd enemy to fight//
     } else if (gameStage === "choose-2nd-enemy") {
       $("#enemyPic").append(this);
       enemyAttr = $(this).data();
-      console.log(enemyAttr);
+      showContent();
       $(this).css("width", "20%");
-      $("#yourEnemy").fadeIn();
-      $("#battle").fadeIn();
-      $("#attack-btn").fadeIn();
-      $("#enemyAttr").text(enemyAttr.name + " / " + "HP= " + enemyAttr.hp);
-      $("#choose").html("Your Enemies Awating For The Next Battle");
+      $("#choose").html("Fight Your Enemy By Clicking The Attack Button!");
+      $("#enemyName").html(enemyAttr.name);
+      $("#enemyAttr").html(enemyAttr.hp);
       gameStage = "battle-2";
 
       //Player fights the last enemy//
     } else if (gameStage === "choose-3rd-enemy") {
       $("#enemyPic").append(this);
       enemyAttr = $(this).data();
-      console.log(enemyAttr);
+      showContent();
       $(this).css("width", "20%");
-      $("#yourEnemy").fadeIn();
-      $("#battle").fadeIn();
-      $("#attack-btn").fadeIn();
-      $("#enemyAttr").text(enemyAttr.name + " / " + "HP= " + enemyAttr.hp);
+      $("#enemyName").html(enemyAttr.name);
+      $("#enemyAttr").html(enemyAttr.hp);
       $("#select-character").fadeOut();
       gameStage = "battle-3";
     }
@@ -101,6 +126,7 @@ $(document).ready(function() {
   //Make sure the attack button triggers only once//
   $("#attack").unbind("click");
   $("#attack").on("click", function() {
+
     //Lightsaber sound effect//
     var attackAudio = document.createElement("audio");
     attackAudio.setAttribute("src", "Assets/audio/fight.mp3");
@@ -108,46 +134,47 @@ $(document).ready(function() {
 
     //First battle//
     if (gameStage === "battle-1") {
-      charAttr.ap = charAttr.ap++;
-      charAttr.hp -= enemyAttr.cap;
-      enemyAttr.hp -= charAttr.ap;
-      console.log(charAttr.hp);
+      inBattle();
+      // console.log(charAttr.hp);
       console.log(enemyAttr.hp);
       if (enemyAttr.hp <= 0) {
         alert("You have won the battle! Choose your next enemy");
         gameStage = "choose-2nd-enemy";
-        $("#enemyPic").empty();
-        $("#yourEnemy").fadeOut();
-        $("#battle").fadeOut();
-      } //Make losing condition if player health >= 0//
+        $("#choose").html("Choose Your Enemy For The Second Battle");
+        removeLoser();
+      } else if (charAttr.hp <=0) {
+        alert("You have been defeated. Choose your opponent wisely...");
+        location.reload();
+      }
 
       //Second battle//
     } else if (gameStage === "battle-2") {
-      charAttr.ap += charAttr.ap;
-      charAttr.hp -= enemyAttr.cap;
-      enemyAttr.hp -= charAttr.ap;
-      console.log(charAttr.hp);
-      console.log(enemyAttr.hp);
+      inBattle();
+      // console.log(charAttr.hp);
+      // console.log(enemyAttr.hp);
       if (enemyAttr.hp <= 0) {
         alert("You have won the battle! Choose your next enemy");
         gameStage = "choose-3rd-enemy";
-        $("#enemyPic").empty();
-        $("#yourEnemy").fadeOut();
-        $("#battle").fadeOut();
+        $("#choose").html("Your Enemy For The Final Battle");
+        removeLoser();
+      } else if (charAttr.hp <=0) {
+        alert("You have been defeated. Choose your opponent wisely...");
+        location.reload();
       }
 
       //Last battle//
     } else if (gameStage === "battle-3") {
-      charAttr.ap += charAttr.ap;
-      charAttr.hp -= enemyAttr.cap;
-      enemyAttr.hp -= charAttr.ap;
-      console.log(charAttr.hp);
-      console.log(enemyAttr.hp);
+      inBattle();
+      // console.log(charAttr.hp);
+      // console.log(enemyAttr.hp);
       if (enemyAttr.hp <= 0) {
         alert("You Brought Justice and Peace to the Galaxy!");
-        $("#yourEnemy").empty();
-        $("#yourEnemy").fadeOut();
-        $("#battle").fadeOut();
+        $("#select-character").fadeIn();
+        $("#choose").html("You Are Victorious! Reload The Page To Play Again");
+        removeLoser();
+      } else if (charAttr.hp <=0) {
+        alert("You have been defeated. Choose your opponent wisely...");
+        location.reload();
       }
     }
   });
